16a7807781a0930a5b0fecd7cd82ae53ee117d78
MahaLakshmi576 <maha71313@gmail.com>
2025-06-03 10:40:19 +0530

    Create Find the smallest of three numbers.py

---
+++ Find the smallest of three numbers.py
@@ -0,0 +1,5 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+smallest = min(a, b, c)
+print(f"The smallest number is: {smallest}")
